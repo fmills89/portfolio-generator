3055338a7052405eef155f4b383db9d2d71680af
FoboIX <89666151+fmills89@users.noreply.github.com>
2021-12-27 15:13:35 -0600

    generate entire website

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
-
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -135,12 +134,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-      const pageHTML = generatePage(portfolioData);
-
-      fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw err;
-
-          console.log("Portfolio complete! Check out index.html to see....");
-      });
-      console.log(portfolioData);
+      return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+      return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+      console.log(writeFileResponse);
+      return copyFile();
+    })
+    .then(copyFileResponse => {
+      console.log(copyFileResponse);
+    })
+    .catch(err => {
+      console.log(err);
     });
